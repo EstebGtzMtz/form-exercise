77d664a6bd782d5675d73c8fb8ea7b99746121f3
EstebGtzMtz <esteb.gtz.mtz@hotmail.com>
2023-09-19 19:19:05 -0600

    add mui and a select input to read information

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
 import './App.css';
-import { useForm } from "react-hook-form";
+import { useForm, SubmitHandler } from 'react-hook-form';
 import { useState } from 'react';
+import { Button, Grid, MenuItem, TextField, Typography } from '../node_modules/@mui/material/index';
 
 interface formObjectInterface {
   id: number,
-  name: string,
-  type: string,
-  value?: string,
-  label?: string
-  validation?: {
-    required: true
-  }
+  name: string
+  type: string
+  options?: number[]
+  defaultSelectOption?: number
 }
+
+type Inputs = {
+  name: string,
+  email: string,
+  password: string
+  age: string
+
+};
 
 
 const formObject : formObjectInterface[] = [
@@ -19,23 +25,22 @@ const formObject : formObjectInterface[] = [
     id: 1,
     name: 'name',
     type: 'text',
-    value: '',
-    label:'',
-    validation:'required'
   },
   {
     id: 2,
     name: 'email',
     type: 'email',
-    value: '',
-    label:''
   },
   {
     id: 3,
     name: 'password',
     type: 'password',
-    value: '',
-    label:''
+  },
+  {
+    id: 4,
+    name: 'age',
+    type: 'select',
+    options: [10, 20, 30]
   }
 ]
 
@@ -43,30 +48,60 @@ const formObject : formObjectInterface[] = [
 function App() {
 
   const [formInfo, setformInfo] = useState({})
-  const { register, handleSubmit } = useForm();
-  const onSubmit = data => setformInfo({...data})
+  const { register, handleSubmit } = useForm({
+    defaultValues: {
+      age: 10,
+    }
+  });
+  const onSubmit: SubmitHandler<Inputs> = (data) => setformInfo({...data})
 
   return (
-    <div>
+  <>
+    <Typography>Print your form information</Typography>
+    <Grid container sx={{display: 'flex', flexDirection: 'column'}}>
       <form onSubmit={handleSubmit(onSubmit)}>
         {
-          formObject.map(el =>(
-            <div key={el.id} className='input-label'>
-              <label htmlFor={el.name}>{el.name}</label>
-              <input
-                key={el.id}
-                type={el.type}
-                {...register(el.name, {required: el.validation?.required})}
-              />
-            </div>
-          ))
+            formObject.map(el => (
+              <Grid item xs={12} key={el.id} sx={{marginTop: 2}}>
+                {
+                  el.type === 'select' ?
+                  <Grid key={el.id}>
+                    <TextField
+                      select
+                      defaultValue=""
+                      label={el.name}
+                      inputProps={register(el.name)}
+                    >
+                      {
+                        el.options.map((el, idx) => (
+                          <MenuItem key={idx} value={el}>{el}</MenuItem>
+                        ))
+                      }
+                    </TextField>
+                  </Grid> :
+                  <TextField
+                    label={el.name}
+                    type={el.type}
+                    name={el.name}
+                    {...register(el.name)}
+                  />
+                }
+              </Grid>
+            ))
         }
-        <button type='submit'>submit</button>
+        <Grid container spacing={2} sx={{mb: 2, mt: 1}}>
+            <Grid item xs={12}>
+              <Button variant='contained' fullWidth type='submit'>
+                Submit
+              </Button>
+            </Grid>
+          </Grid>
       </form>
-      {
-        JSON.stringify(formInfo)
-      }
-    </div>
+    </Grid>
+      <pre>
+        {JSON.stringify(formInfo, null, 2)}
+      </pre>
+  </>
   )
 }
 
